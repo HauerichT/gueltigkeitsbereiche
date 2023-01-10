4ba6cd08f397e6fb6bbd5867a092c138ee680fa7
timohaverich <timo.haverich@fh-bielefeld.de>
2023-01-10 17:05:55 +0100

    init

--- main.py
+++ main.py
@@ -7,33 +7,33 @@ def scopes():
     def do_local():
         wort = "local wort"
         print("in do-local: " + wort)
-        print_scopes()
 
     def do_nonlocal():
         nonlocal wort
         wort = "nonlocal wort"
         print("in do-nonlocal: " + wort)
-        print_scopes()
 
     def do_global():
         global wort
         wort = "global wort"
         print("in do-global: " + wort)
-        print_scopes()
 
     def print_scopes():
         print("in scopes: " + wort)
 
-    print_scopes()
-    do_local()
-    do_nonlocal()
-    do_global()
+    for i in range(4):
+        if i == 1:
+            do_local()
+        if i == 2:
+            do_nonlocal()
+        if i == 3:
+            do_global()
+        print("in scopes: " + wort)
 
 
-for i in range(3):
-    if i == 0 or i == 2:
-        print("im Hauptprogramm: " + wort)
+for i in range(2):
     if i == 1:
         scopes()
+    print("im Hauptprogramm: " + wort)
 
 
